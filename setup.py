9a4d1b7c153c62a3e90d3fbde74508244484c4ce
Roberto Carreiro <robertopcarreiro@users.noreply.github.com>
2025-04-04 10:19:54 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cidacs.geolinkage",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Biblioteca fake para validação no Singularity",
+    author="CIDACS",
+    author_email="contato@cidacs.org",
+    url="https://github.com/seuusuario/cidacs-geolinkage",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
